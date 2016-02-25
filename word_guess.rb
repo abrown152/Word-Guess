e23a5eb1d2093062ce2b075ea5a21968cd900f15
Mindy Carson <mcarson1111@gmail.com>
2016-02-25 15:47:31 -0800

    Added flowers and output

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,21 @@
-word_array = ["dog", "cat", "pet", "hut"]
+word_array = ["DOG", "CAT", "PET", "HUT"]
 word = word_array.sample              #chooses a random word
 letter_array = word.split("")         #splits the random word's letter into an array
+
+dash_array = [ " _ ", " _ ", " _ "]
+
+#The animation goes here....
+puts "\n
+ (@)(@)(@)(@)(@)
+    \ \ | / /
+      _\|/_
+     |_____|
+      |   |
+      |___| "         #why is \ registering as a command within this string????
+#this prints out the dash array
+puts dash_array.join
+
+puts "\nPlease choose a letter and press enter!"
+  user_letter = gets.chomp.upcase
+
+puts "You selected #{user_letter}."
