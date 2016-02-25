98b19ba85da5f4ba0ff66c6c3ec68359e1331e87
Alysia Brown <alysiacbrown@gmail.com>
2016-02-25 14:37:52 -0800

    Added word and letter arrays.

--- word_guess.rb
+++ word_guess.rb
@@ -1,1 +1,3 @@
-# Hey girl ^_^
+word_array = ["dog", "cat", "pet", "hut"]
+word = word_array.sample              #chooses a random word
+letter_array = word.split("")         #splits the random word's letter into an array
