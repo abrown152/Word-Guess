b27b7dde79dc99a88bca2fff464a97462286bc2a
Alysia Brown <alysiacbrown@gmail.com>
2016-02-25 16:33:10 -0800

    Fixed if/else statements for checking letter validity.

--- word_guess.rb
+++ word_guess.rb
@@ -2,6 +2,8 @@ word_array = ["DOG", "CAT", "PET", "HUT"]
 word = word_array.sample              #chooses a random word
 letter_array = word.split("")         #splits the random word's letter into an array
 
+puts word
+
 dash_array = [ " _ ", " _ ", " _ "]
 
 letters_guessed = []
@@ -22,24 +24,24 @@ puts "\nPlease choose a letter and press enter!"
 
 puts "You selected #{user_letter}."
 
-letters_guessed << user_letter    #Adds the letter user guessed to the array
-                                  #of letters user has already guessed.
 puts "Letters Guessed:"
-puts letters_guessed.join
+puts letters_guessed.join                        #of letters user has already guessed.
 
-def include_in_guessed
+#def include_in_guessed
   if letters_guessed.include?(user_letter) == true
     puts "You already guessed that letter! Try again."
   else
-    include_in_word
-  end
-end
+#    include_in_word
+#  end
+#end
 
-def include_in_word
-  if letter_array.include?(user_letter) == false
-    puts "#{user_letter} is not included in the word!"
-  else
-    puts "You guessed correctly!"
+#def include_in_word
+    if letter_array.include?(user_letter) == false
+      puts "#{user_letter} is not included in the word!"
+    else
+      puts "You guessed correctly!"
     #Enter code that changes the dash to the letter the user guessed right heeere
+    end
   end
-end
+
+  letters_guessed << user_letter    #Adds the letter user guessed to the array
